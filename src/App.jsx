af2c7b69aebe22047e4978d0ba472b0e5545b735
So Youn Choi <manghatuhyo@gmail.com>
2023-10-05 18:51:26 +0100

    showing api text by iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return <div>
+    <iframe 
+    src="https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts" 
+    style={{border: 'none', width: '100%', height: 350}}>
+      </iframe>
+      </div>;
 };
