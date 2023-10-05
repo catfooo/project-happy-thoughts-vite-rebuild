b5aad9b740bd9215736bcb313786b8cfd4e8009c
So Youn Choi <manghatuhyo@gmail.com>
2023-10-05 21:10:34 +0100

    not working fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 export const App = () => {
+  // async function getResult() {
+  //   const response = await fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts")
+  //   const result = await response.json()
+  //   console.log(result)
+  }
   return <div>
-    <iframe 
+    {/* <iframe 
     src="https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts" 
     style={{border: 'none', width: '100%', height: 350}}>
-      </iframe>
+      </iframe> */}
       </div>;
 };
